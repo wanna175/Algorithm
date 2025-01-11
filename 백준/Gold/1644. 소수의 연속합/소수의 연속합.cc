f3254515cf48bbaf25f476aff271645c98088808
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-01-11 18:06:27 +0900

    [Gold III] Title: 소수의 연속합, Time: 20 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <memory.h>
+using namespace std;
+const int s = 4000001;
+bool isPrime[s];
+int N,ret=0;
+void Sieve_of_Eratosthenes() {
+	memset(isPrime, true, s);
+	isPrime[1] = false;
+	int n = 1;
+	while (n<=sqrt(N)) {
+		++n;
+		if (!isPrime[n])
+			continue;
+		for (int i = 2; n * i <= N; ++i)
+			isPrime[n * i] = false;
+	}
+}
+
+void solve(int left, int right,int cur) {
+	if (cur == N)
+		++ret;
+	if (right > N)
+		return;
+	if (cur <= N) {//right 인덱스 증가
+		while (true) {
+			++right;
+			if (isPrime[right]) {
+				cur += right;
+				break;
+			}
+		}
+	}
+	else if(cur > N) {//left 인덱스 증가
+		cur -= left;
+		while (true) {
+			++left;
+			if (isPrime[left]) {
+				break;
+			}
+		}
+	}
+	solve(left, right, cur);
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	Sieve_of_Eratosthenes();
+	solve(2, 2, 2);
+	cout << ret;
+	return 0;
+}
