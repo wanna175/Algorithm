8b000202d4df385e8a833bb7483939dfc09e0edb
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-05-27 12:58:13 +0900

    [Silver III] Title: 모든 순열, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.cc
+++ 백준/Silver/10974. 모든 순열/모든 순열.cc
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <set>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int N;
-vector<int> vec;
+vector<int> A;
 int main(void) {
         cin >> N;
-        for (int i=1;i<=N;++i)
-                vec.push_back(i);
-        do{
-                for (const auto& a : vec) {
-                        cout << a <<' ';
-                }
+        for (int i=0;i<N;++i)
+                A.push_back(i+1);
+
+        do {
+                for (auto it = A.begin(); it != A.end(); ++it)
+                        cout << *it << ' ';
                 cout << '\n';
-        }while(next_permutation(vec.begin(),vec.end()));
+        } while(next_permutation(A.begin(),A.end()));
         return 0;
 }
