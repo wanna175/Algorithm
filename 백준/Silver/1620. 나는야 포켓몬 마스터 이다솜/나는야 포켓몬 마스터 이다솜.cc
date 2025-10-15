baf1906166ebe67faf131d1b953dd823a6931add
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-10-15 09:42:51 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 132 ms, Memory: 20256 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,31 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int N, M;
-	cin >> N >> M;
-	vector<string> pokemon(N+1,"");
-	map<string, int> pokemon_num;
-	vector<string> answer(M, "");
-	for (int i = 1; i <= N; ++i) {
-		cin >> pokemon[i];
-		pokemon_num[pokemon[i]] = i;
+int N,M;//1~100000
+vector<string> v;
+map<string,int> m;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin>>N>>M;
+	v.push_back("-1");
+	for(int i=0;i<N;++i){
+		string s;
+		cin>>s;
+		v.push_back(s);
+		m[s] = i+1;
 	}
-	for (int i = 0; i < M; ++i)
-		cin >> answer[i];
-	for (int i = 0; i < M; ++i) {
-		if (isdigit(answer[i][0])) {
-			int tmp = stoi(answer[i]);
-			cout<< pokemon[tmp]<<'\n';
-		}
-		else {
-			cout<<pokemon_num[answer[i]]<<'\n';
+	for(int i=0;i<M;++i){
+		string s;
+		cin>>s;
+		if(atoi(s.c_str()) == 0){
+			cout << m[s] << '\n';
+		}else{
+			cout << v[atoi(s.c_str())] << '\n';
 		}
 	}
 	return 0;
