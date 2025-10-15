42cefb35602097429303853d062415b8f139cf27
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-10-15 15:35:20 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,19 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 int n;
-int main(void) {
-        while(true) {
-                cin >> n;
-                if (cin.eof()) break;
-                int i=1;
-                int ret=1;
-                while(true) {
-                        if (i%n==0) break;
-                        i = 10*i + 1;
-                        while(i>n) {i-=n;}
-                        ret++;
-                }
-                cout << ret <<'\n';
-        }
-        return 0;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	while(true){
+		cin>>n;
+		if(cin.eof())break;
+		int i=1;
+		int ret=1;
+		while(true){
+			if(i%n==0) break;
+			i = 10*i +1;
+			i%=n;
+			ret++;
+		}
+		cout << ret << '\n';
+	}
+	return 0;
 }
