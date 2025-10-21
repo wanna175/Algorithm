fbd64a47b7061a408d295e380370be0a228da676
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-10-21 13:41:18 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,54 +1,51 @@
-#include<iostream>
-#include<vector>
-#define pii pair<int,int>
+#include <bits/stdc++.h>
+
 using namespace std;
-int N, M;
-int board[51][51] = { 0 };
-vector<pii> chicken;
-vector<pii> house;
-int get_chicken(){
-	int ret=0;
-	for (int i = 0; i < house.size(); ++i) {
-		int tmp = 5000;
-		for (int j = 0; j < chicken.size(); ++j) {
-			int cy = chicken[j].first;
-			int cx = chicken[j].second;
-			if (board[cy][cx] == 0) continue;
-			int y = house[i].first;
-			int x = house[i].second;
-			tmp = min(tmp, abs(y - cy) + abs(x - cx));
+typedef pair<int,int> pii;
+
+const int INF = 1e8;
+int N,M,ret = INF;//2-50
+int a[54][54];
+vector<pii> v;
+vector<pii> sel;
+int sumd(){
+	int r=0;
+	for(int i=0;i<N;++i){
+		for(int j=0;j<N;++j){
+			if(a[i][j]!=1) continue;
+			int s = INF;
+			for(auto k : sel){
+				int d = abs(i-k.first)+abs(j-k.second);
+				s=min(s,d);
+			}
+			r+=s;
 		}
-		ret += tmp;
 	}
-	return ret;
+	return r;
 }
-int solve(int toPick,int idx) {
-	if (toPick == 0)
-		return get_chicken();
-	int ret = 5000;
-	for (int i = idx; i < chicken.size(); ++i) {
-		int y = chicken[i].first;
-		int x = chicken[i].second;
-		board[y][x] = 0;
-		ret = min(ret,solve(toPick - 1, i + 1));
-		board[y][x] = 2;
+void solve(int idx,int size){
+	if (size == sel.size()){
+		ret = min(ret,sumd());
+		return;
 	}
-	return ret;
+	if(idx >=v.size()) return;
+	solve(idx+1,size);
+	sel.push_back(v[idx]);
+	solve(idx+1,size);
+	sel.pop_back();
 }
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> N >> M;
-	for(int i=1;i<=N;++i)
-		for (int j = 1; j <= N; ++j) {
-			cin >> board[i][j];
-			if (board[i][j] == 2)
-				chicken.push_back({ i,j });
-			else if (board[i][j] == 1)
-				house.push_back({ i,j });
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>N>>M;
+	for(int i=0;i<N;++i){
+		for(int j=0;j<N;++j){
+			cin>>a[i][j];
+			if(a[i][j]==2) v.push_back({i,j});
 		}
-	int n = chicken.size() - M;
-	cout << solve(n, 0);
+	}
+	solve(0,M);
+
+	cout << ret <<'\n';
 	return 0;
 }
