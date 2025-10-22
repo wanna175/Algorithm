d913dbb18bf28bb1744b80bda43493751a1c70bb
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-10-22 10:37:34 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 13968 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,76 +1,66 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<algorithm>
-using namespace std;
+#include <bits/stdc++.h>
 
-int w, h;
-char board[1002][1002];
-int cnt[1002][1002];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-int BFS(const vector<pair<int, int>>& pos, int y, int x) {
-	queue<pair<int, int>> Q;
-	Q.push({ y,x });
-	cnt[y][x] = 1;
-	for (auto v : pos) {
-		Q.push({ v.first,v.second });
-	}
-	while (!Q.empty()) {
-		int cy = Q.front().first;
-		int cx = Q.front().second;
-		Q.pop();
-		for (int i = 0; i < 4; ++i) {
-			int ny = cy + dy[i];
-			int nx = cx + dx[i];
-			if (ny < 1 || nx < 1 || ny > h || nx > w) continue;
-			if (board[ny][nx] == 'F' || board[ny][nx] == '#') continue;
-			if (board[cy][cx] == 'J') {
-				if (board[ny][nx] == 'J')
-					continue;
-				int flag = false;
-				for (int j = 0; j < 4; ++j) {
-					if (board[ny + dy[j]][nx + dx[j]] == 'F')
-						flag = true;
-				}
-				if (flag) continue;
-				cnt[ny][nx] = cnt[cy][cx] + 1;
-				if (ny == 1 || ny == h || nx == 1 || nx == w)
-					return cnt[ny][nx];
+using namespace std;
+typedef pair<int,int> pii;
+int dy[] = {1,0,-1,0};
+int dx[] = {0,1,0,-1};
+int R,C,ret;
+int a[1004][1004];
+int visited[1004][1004], visited_j[1004][1004];
+deque<pii> dq;
+int BFS(){
+	while(!dq.empty()){
+		int cy,cx;
+		tie(cy,cx) = dq.front();dq.pop_front();
+		for(int i=0;i<4;++i){
+			int ny = cy+dy[i];
+			int nx = cx+dx[i];
+			if(ny<0||nx<0||ny>=R||nx>=C){
+				if(a[cy][cx] == 2) return visited_j[cy][cx];
+				continue;
 			}
-			Q.push({ ny,nx });
-			board[ny][nx] = board[cy][cx];
+			if (a[cy][cx] == 2){
+				if(visited_j[ny][nx]) continue;
+				if(a[ny][nx]!=0) continue;
+				visited_j[ny][nx] = visited_j[cy][cx]+1;
+				
+				a[ny][nx] = 2;
+				dq.push_back({ny,nx});
+			}
+			else{
+				if(visited[ny][nx]) continue; 
+				if(a[ny][nx]==-1) continue;
+				a[ny][nx] = 1;
+				visited[ny][nx] = 1; 
+				dq.push_back({ny,nx});
+			}
 		}
 	}
 	return -1;
 }
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> h >> w;
-	int y = -1, x = -1;
-	vector<pair<int, int>> fire;
-	for (int i = 1; i <= h; ++i) {
-		for (int j = 1; j <= w; ++j) {
-			cin >> board[i][j];
-			cnt[i][j] = 0;
-			if (board[i][j] == 'J') {
-				y = i; x = j;
-				cnt[y][x] = 1;
-			}
-			else if (board[i][j] == 'F') {
-				fire.push_back({ i,j });
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>R>>C;
+	for(int i=0;i<R;++i){
+		string s;
+		cin>>s;
+		for(int j=0;j<C;++j){
+			if(s[j]=='#') a[i][j] = -1;
+			else if(s[j]=='J'){
+				a[i][j] = 2;
+				dq.push_front({i,j});	
+				visited_j[i][j] = 1;
+			} 
+			else if(s[j]=='F'){
+				a[i][j] = 1;
+				dq.push_back({i,j});
+				visited[i][j] = 1;
 			}
 		}
 	}
-	int answer = 0;
-	if (y == 1 || y == h || x == 1 || x == w)
-		answer = 1;
-	else
-		answer = BFS(fire, y, x);
-	if (answer == -1)
-		cout << "IMPOSSIBLE\n";
-	else
-		cout << answer << '\n';
+	ret = BFS();
+	if(ret!=-1) cout << ret << '\n';
+	else cout << "IMPOSSIBLE" << '\n';
 	return 0;
 }
