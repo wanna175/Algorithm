e2b14373028e87ff08a5c09cad529c1acdb2d89b
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-11-14 10:43:20 +0900

    [Gold V] Title: AC, Time: 36 ms, Memory: 4092 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,62 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <bits/stdc++.h>
+
 using namespace std;
-vector<int> split(string str, char delimiter) {
-	vector<int> str_arr;
-	istringstream ss(str);
-	string buffer;
-	while (getline(ss, buffer, delimiter))
-		str_arr.emplace_back(stoi(buffer));
-	return str_arr;
-}
-string solve(string vec, string fuc) {
-	string ret = "";
-	vector<int> arr = split(vec, ',');
-	int cnt = 0,first=0,end=arr.size();
-	bool reverse = false;
-	while (cnt < fuc.size()) {
-		if (fuc[cnt] == 'R')
-			reverse = !reverse;
-		else {
-			if (first==end)
-				return "error";
-			(!reverse) ? ++first : --end;
-		}
-		++cnt;
-	}
-	if(reverse){
-		for (int i = end-1; i >= first; --i)
-			ret += to_string(arr[i]) + ",";
-	}
-	else {
-		for (int i = first; i < end; ++i)
-			ret += to_string(arr[i]) + ",";
-	}
-	if(ret.size()!=0)
-		ret.pop_back();
+int t,n,head,tail;
+string p,str;
+bool f;
+vector<int> split(const string& input,string delimiter){
+	vector<int> ret;
+	if(!input.size()) return ret;
+	auto start = 0;
+	auto end = input.find(delimiter);
+	while(end != string::npos){
+		ret.push_back(stoi(input.substr(start,end-start)));
+		start = end+delimiter.size();
+		end = input.find(delimiter,start);
+	} 
+	ret.push_back(stoi(input.substr(start)));
 	return ret;
 }
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int T;
-	cin >> T;
-	vector<string> answer(T, "[");
-	for (int i = 0; i < T; ++i) {
-		string tmp,arr;
-		int n;
-		cin >> tmp >> n >> arr;
-		arr = arr.substr(1, arr.size() - 2);
-		string ret = solve(arr, tmp);
-		if (ret.compare("error")==0)
-			answer[i] = ret;
-		else
-			answer[i] += ret + "]";
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>t;
+	while(t--){
+		cin>>p>>n>>str;
+		vector<int> v = split(str.substr(1,str.size()-2),",");
+		head = 0;tail=n;f=0;
+		for(int i=0;i<p.size();++i){
+			if(p[i]=='R'){
+				if(head>tail){
+					head++;tail++;
+				}
+				else{
+					head--;tail--;
+				}
+				swap(head,tail);
+			}else{
+				if(head == tail){
+					f=1;break;
+				}
+				else if(head>tail) head--;
+				else head++;
+			}
+		}
+		if(f){
+			cout << "error\n";
+			continue;
+		}
+		string ret = "[";
+		if(head>tail){
+			for(int i = head;i>tail;--i){
+				ret+=to_string(v[i])+",";
+			}
+		}
+		else {
+			for(int i = head;i<tail;++i){
+				ret+=to_string(v[i])+",";
+			}
+		}
+		if(ret.size()!=1) ret.pop_back();
+		ret+="]\n";
+		cout << ret;
 	}
-	for (int i = 0; i < T; ++i)
-		cout << answer[i] << '\n';
 	return 0;
 }
