59e39fe51cd3f43fa50ac1ccdc8e913e89453832
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-11-19 09:32:37 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string s;
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>s;
+	cout << s <<"\?\?\!" << '\n';
+	return 0;
+}
