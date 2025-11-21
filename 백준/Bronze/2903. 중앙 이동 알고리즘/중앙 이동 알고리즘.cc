725c39dbcda622745186ec4a77fee4c7401d99de
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-11-21 10:05:45 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>n;
+	cout << (long long)pow(pow(2,n)+1,2) <<'\n';
+	return 0;
+} 
