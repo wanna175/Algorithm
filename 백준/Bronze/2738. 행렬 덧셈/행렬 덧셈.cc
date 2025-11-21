aec22ec1fbb5957e025187cf92ed36fb091ac488
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-11-21 09:56:58 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n,m,a[104][104],b[104][104];
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>n>>m;
+	for(int i=0;i<n;++i){
+		for(int j=0;j<m;++j){
+			cin>>a[i][j];
+		}
+	}
+	for(int i=0;i<n;++i){
+		for(int j=0;j<m;++j){
+			cin>>b[i][j];
+			a[i][j]+=b[i][j];
+		}
+	}
+	for(int i=0;i<n;++i){
+		for(int j=0;j<m;++j){
+			cout << a[i][j] << " ";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
