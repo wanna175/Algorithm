bbd4ce20270ea1f722f534002fc422ea7734b34b
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-12-01 11:09:59 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -7,33 +7,10 @@ int main(){
 	cin.tie(NULL);cout.tie(NULL);
 	while(1){
 		cin>>a>>b;
-		if(a==0&&b==0) break;
-		
-		if(a>b){
-			int sum=b;
-			for(int i=1;i<10000;++i){
-				sum+=b;
-				if(sum==a){
-					cout << "multiple\n";
-					break;
-				}else if(sum>a){
-					cout << "neither\n";
-					break;
-				}
-			}
-		}else{
-			int sum=a;
-			for(int i=1;i<10000;++i){
-				sum+=a;
-				if(sum==b){
-					cout << "factor\n";
-					break;
-				}else if(sum>b){
-					cout << "neither\n";
-					break;
-				}
-			}
-		}
+		if(a==0&&b==0) break;			
+		if(a>b&&a%b==0)cout << "multiple\n";
+		else if(b>a&&b%a==0)cout << "factor\n";
+		else cout << "neither\n";		
 	}
 	return 0;
 }
