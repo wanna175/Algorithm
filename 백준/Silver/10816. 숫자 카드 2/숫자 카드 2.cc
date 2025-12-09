1c0f5b258833b4c454c2909c343003301df238a2
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-12-09 10:20:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 632 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n,m,a;
+map<int,int> v;
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>n;
+	for(int i=0;i<n;++i){
+		cin>>a;
+		v[a]++;
+	}
+	cin>>m;
+	for(int i=0;i<m;++i){
+		cin>>a;
+		if(v.find(a)!=v.end()){
+			cout << v[a]<<" ";
+		}else cout << 0 << " ";
+	}
+	return 0;
+}
