a7fb0e6359b3a482b3228f4f721dbae39ff645b8
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-12-11 10:51:24 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);cout.tie(NULL);
+	cin>>n;
+	cout << (int)sqrt(n);
+	return 0;
+}
