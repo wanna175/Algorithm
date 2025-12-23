18156d3f3e19752e695214cb35c87118add8c9f8
qwe541 <141459976+wanna175@users.noreply.github.com>
2025-12-23 14:01:58 +0900

    [Gold III] Title: 순회강연, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -2,31 +2,29 @@
 
 using namespace std;
 typedef pair<int,int> pii;
-int n,ret;
-vector<pii> v;
 priority_queue<int> pq;
+vector<pii> v;
+int n,ret,p,d;
 int main(){
 	ios_base::sync_with_stdio(0);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(NULL);cout.tie(NULL);
 	cin>>n;
-	for(int i=0;i<n;++i) {
-		int d,p;
+	for(int i=0;i<n;++i){
 		cin>>p>>d;
 		v.push_back({d,p});
 	}
 	sort(v.begin(),v.end(),greater<pii>());
-
 	int j=0;
 	for(int i=10001;i>0;--i){
-		while(j<n&&i<=v[j].first) {
+		while(j<n&&v[j].first>=i){
 			pq.push(v[j].second);
-			++j; 
+			j++;
 		}
-		if(pq.size()){
-			ret+=pq.top();pq.pop();	
+		if(!pq.empty()){
+		    ret += pq.top();
+		    pq.pop();
 		}
 	}
-	cout << ret << '\n';	
-	
+	cout << ret;
 	return 0;
 }
